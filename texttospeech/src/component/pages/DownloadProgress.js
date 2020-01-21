61558a35346eb28eecd3e9cf474a0f49370cbec5
jiajunmao <aaronmao.hangzhou@gmail.com>
2020-01-20 23:14:12 -0500

    fixing merge issue

--- texttospeech/src/component/pages/DownloadProgress.js
+++ texttospeech/src/component/pages/DownloadProgress.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import {Container} from "react-bootstrap";
-<<<<<<< HEAD
-import LinearProgress from '@material-ui/core/LinearProgress'
-import './DownloadProgress.css'
-
-class LinearDeterminate extends React.Component {
-=======
 import {makeStyles} from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress'
 import './DownloadProgress.css'
 
 class LinearDeterminate extends React.Component{
->>>>>>> 31bbd7023fd57a9dcd1ac263d9b6426e28ab59d2
     constructor() {
         super()
         this.state = {
@@ -22,11 +15,7 @@ class LinearDeterminate extends React.Component{
     }
 
     step = () => {
-<<<<<<< HEAD
-        const {completed} = this.state;
-=======
         const { completed } = this.state;
->>>>>>> 31bbd7023fd57a9dcd1ac263d9b6426e28ab59d2
         var newCompleted = completed + 1;
         if (completed > 100) {
             this.setState({completed: 100})
